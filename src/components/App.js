a967616726e310fc70bdc23b095d25b42d11f50a
Maksymilian Olszewski <molszewski@itsg.com.pl>
2018-11-14 10:30:59 +0100

    created higher order component and connected it to redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  )
+class App extends Component {
+  onAuthClick(isLoggedIn) {
+    this.props.changeAuth(!isLoggedIn);
+  }
+
+  renderButton() {
+    return (
+      <button onClick={() => this.onAuthClick(this.props.auth)}>
+        {this.props.auth ? 'Sing out' : 'Sing in'}
+      </button>
+    )
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/" exact component={CommentList} />
+        <Route path="/post" exact component={CommentBox} />
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps, actions)(App);
